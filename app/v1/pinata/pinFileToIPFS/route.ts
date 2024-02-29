c190a7d5d8c202791d52789f32bb2f00911b5dd6
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-02-29 18:38:06 -0300

    :bug: test as text

--- app/v1/pinata/pinFileToIPFS/route.ts
+++ app/v1/pinata/pinFileToIPFS/route.ts
@@ -9,30 +9,30 @@ export const runtime = "nodejs";
 const pinata = new pinataSDK({ pinataJWTKey: env.PINATA_JWT });
 
 export async function POST(request: Request) {
-  const formData = await request.blob();
+  const formData = await request.text();
 
   console.log("FormData:", formData);
 
-  const stream = await formData.arrayBuffer();
+  // const stream = await formData.arrayBuffer();
 
-  const buffer = Buffer.from(stream);
+  // const buffer = Buffer.from(formData);
 
-  const boundary = parse.getBoundary(formData.type);
-  const parts = parse.Parse(buffer, boundary);
+  // const boundary = parse.getBoundary(formData.type);
+  // const parts = parse.Parse(buffer, boundary);
 
-  if (parts.length === 0) {
-    return NextResponse.json({ error: "No parts found" }, { status: 400 });
-  }
+  // if (parts.length === 0) {
+  //   return NextResponse.json({ error: "No parts found" }, { status: 400 });
+  // }
 
-  const part = parts[0];
-  console.log("Part:", part);
+  // const part = parts[0];
+  // console.log("Part:", part);
 
-  const resPinata = await pinata.pinFileToIPFS(
-    Stream.Readable.from(part.data),
-    {
-      pinataMetadata: { name: part.filename },
-    }
-  );
+  // const resPinata = await pinata.pinFileToIPFS(
+  //   Stream.Readable.from(part.data),
+  //   {
+  //     pinataMetadata: { name: part.filename },
+  //   }
+  // );
 
-  return NextResponse.json(resPinata, { status: 200 });
+  // return NextResponse.json(resPinata, { status: 200 });
 }
