f0a3b4fa641a231100d8c5cef41ffc43936e927e
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-02-29 16:11:38 -0300

    :bug: change response

--- app/v1/pinata/pinFileToIPFS/route.ts
+++ app/v1/pinata/pinFileToIPFS/route.ts
@@ -1,7 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import pinataSDK from "@pinata/sdk";
 import { env } from "~/env.mjs";
 import { Stream } from "stream";
+import { NextApiResponse } from "next";
 
 const pinata = new pinataSDK({ pinataJWTKey: env.PINATA_JWT });
 
@@ -10,20 +11,16 @@ export async function POST(request: NextRequest) {
     const formData = await request.formData();
 
     let f = formData.get("file") as File | null | undefined;
-
     if (!f) {
-      return Response.json({ error: "No file found" }, { status: 400 });
+      return NextResponse.json({ error: "No file found" }, { status: 400 });
     }
     const streamReads = Stream.Readable.from(f.stream());
 
-    const response = await pinata.pinFileToIPFS(streamReads, {
+    const resPinata = await pinata.pinFileToIPFS(streamReads, {
       pinataMetadata: { name: f.name },
     });
-
-    return Response.json(response, {
-      status: 200,
-    });
+    return NextResponse.json(resPinata, { status: 200 });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
