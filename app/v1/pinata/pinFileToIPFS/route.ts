3b5c03c648157193dd3d7165d93b7846b594f2d1
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-02-29 17:38:54 -0300

    :bug: form data

--- app/v1/pinata/pinFileToIPFS/route.ts
+++ app/v1/pinata/pinFileToIPFS/route.ts
@@ -5,52 +5,6 @@ import { Stream } from "stream";
 
 const pinata = new pinataSDK({ pinataJWTKey: env.PINATA_JWT });
 
-async function parseMultipartFormData(request: NextRequest) {
-  const body = request.body;
-
-  if (!body) {
-    return NextResponse.json({ error: "No form data found" }, { status: 400 });
-  }
-  console.log(body);
-
-  const reader = body.getReader();
-
-  let formDataString = "";
-
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) break;
-    formDataString += new TextDecoder().decode(value);
-  }
-
-  console.log(formDataString);
-
-  const boundary = formDataString.split("\n")[0].trim();
-
-  const parts = formDataString.split(boundary);
-  const formData = new FormData();
-
-  parts.forEach((part) => {
-    if (part.includes("filename")) {
-      const nameMatch = part.match(/name="([^"]+)"/);
-      const filenameMatch = part.match(/filename="([^"]+)"/);
-      const contentTypeMatch = part.match(/Content-Type: ([^\s]+)/);
-
-      if (nameMatch && filenameMatch && contentTypeMatch) {
-        const name = nameMatch[1];
-        const filename = filenameMatch[1];
-        const contentType = contentTypeMatch[1];
-        let content = part.split("\n").slice(4, -2).join("\n"); // Extracting content
-
-        const blob = new Blob([content], { type: contentType });
-        formData.append(name, blob, filename);
-      }
-    }
-  });
-
-  return formData;
-}
-
 export async function POST(request: NextRequest) {
   try {
     // Parse the string into FormData
