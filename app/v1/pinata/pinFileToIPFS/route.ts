f859cba8e2a3723ba1d4dd5e1a072ada7ba90063
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-02-29 16:53:05 -0300

    :debug: logs

--- app/v1/pinata/pinFileToIPFS/route.ts
+++ app/v1/pinata/pinFileToIPFS/route.ts
@@ -7,18 +7,39 @@ const pinata = new pinataSDK({ pinataJWTKey: env.PINATA_JWT });
 
 export async function POST(request: NextRequest) {
   // try {
-  const formData = await request.formData();
+  const formData = request.body;
 
-  let f = formData.get("file") as File | null | undefined;
-  if (!f) {
-    return NextResponse.json({ error: "No file found" }, { status: 400 });
+  if (!formData) {
+    return NextResponse.json({ error: "No form data found" }, { status: 400 });
   }
-  const streamReads = Stream.Readable.from(f.stream());
+  console.log(formData);
 
-  const resPinata = await pinata.pinFileToIPFS(streamReads, {
-    pinataMetadata: { name: f.name },
-  });
-  return NextResponse.json(resPinata, { status: 200 });
+  const reader = formData.getReader();
+
+  let result = "";
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    result += new TextDecoder().decode(value);
+  }
+
+  console.log(result);
+
+  // const streamReads = Stream.Readable.from(formData);
+  // const formData = await request.formData();
+
+  // let f = formData.get("file") as File | null | undefined;
+  // if (!f) {
+  // return NextResponse.json({ error: "No file found" }, { status: 400 });
+  // }
+  // const streamReads = Stream.Readable.from(f.stream());
+
+  // const resPinata = await pinata.pinFileToIPFS(streamReads, {
+  // pinataMetadata: { name: f.name },
+  // });
+  // return NextResponse.json(resPinata, { status: 200 });
+  return NextResponse.json({}, { status: 200 });
   // } catch (error) {
   // console.error(error);
   // return NextResponse.json({ error }, { status: 500 });
