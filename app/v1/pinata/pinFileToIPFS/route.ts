e9bea84adfd493f5c9ee031b663faadbfd66a287
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-02-29 17:39:13 -0300

    :bug: form data

--- app/v1/pinata/pinFileToIPFS/route.ts
+++ app/v1/pinata/pinFileToIPFS/route.ts
@@ -6,29 +6,29 @@ import { Stream } from "stream";
 const pinata = new pinataSDK({ pinataJWTKey: env.PINATA_JWT });
 
 export async function POST(request: NextRequest) {
-  try {
-    // Parse the string into FormData
-    // const formData = await parseMultipartFormData(request);
-    const formData = await request.formData();
+  // try {
+  // Parse the string into FormData
+  // const formData = await parseMultipartFormData(request);
+  const formData = await request.formData();
 
-    // Get the file from FormData
-    const file = formData.get("file");
+  // Get the file from FormData
+  const file = formData.get("file");
 
-    console.log("File:", file);
+  console.log("File:", file);
 
-    let f = formData.get("file") as File | null | undefined;
-    if (!f) {
-      return NextResponse.json({ error: "No file found" }, { status: 400 });
-    }
-    const streamReads = Stream.Readable.from(f.stream());
-
-    const resPinata = await pinata.pinFileToIPFS(streamReads, {
-      pinataMetadata: { name: f.name },
-    });
-    return NextResponse.json(resPinata, { status: 200 });
-    // return NextResponse.json({ result: formDataString }, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error }, { status: 500 });
+  let f = formData.get("file") as File | null | undefined;
+  if (!f) {
+    return NextResponse.json({ error: "No file found" }, { status: 400 });
   }
+  const streamReads = Stream.Readable.from(f.stream());
+
+  const resPinata = await pinata.pinFileToIPFS(streamReads, {
+    pinataMetadata: { name: f.name },
+  });
+  return NextResponse.json(resPinata, { status: 200 });
+  // return NextResponse.json({ result: formDataString }, { status: 200 });
+  // } catch (error) {
+  // console.error(error);
+  // return NextResponse.json({ error }, { status: 500 });
+  // }
 }
